8cea1d557552ad5c6c2ca9637a3329c86e1792ab
Diego Quintela <diegolucasq@gmail.com>
2022-02-05 19:34:40 -0500

    improved UserItem and UserLIst components

--- client/src/users/components/UserItem.js
+++ client/src/users/components/UserItem.js
@@ -4,9 +4,16 @@ import classes from './UserItem.module.css';
 
 const UserItem = (props) => {
   return (
-    <li key={props.id}>
-      <h2>{props.name}</h2>
-      <p>{props.placeCount}</p>
+    <li className={classes.item} key={props.id}>
+      <div className={classes.content}>
+        <div className={classes.image}>
+          <img src={props.image} alt={props.name} />
+        </div>
+        <div className={classes.info}>
+          <h2>{props.name}</h2>
+          <h3>{props.placeCount} {props.placeCount === 1 ? 'Place' : 'Places' </h3>
+        </div>
+      </div>
     </li>
   );
 };
